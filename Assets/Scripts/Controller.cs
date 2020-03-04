c378ce959658250941688ce1085d9a30df8d48c1
igorbrito97 <igorbritto97@hotmail.com>
2020-03-04 09:21:36 -0300

    interface configuration

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Controller : MonoBehaviour
+{
+    private string scene,weather,time;
+
+    [SerializeField] private Dropdown dropdownScene;
+    [SerializeField] private Dropdown dropdownWeather;
+    [SerializeField] private Dropdown dropdownTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NAO USAR O PLAYERPREFABS, 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void click()
+    {
+        weather = dropdownWeather.options[dropdownWeather.value].text;
+        scene = dropdownScene.options[dropdownScene.value].text;
+        time = dropdownTime.options[dropdownTime.value].text;
+        Debug.Log(weather + " " + scene + " " + time);
+        SceneManager.LoadScene(scene + "Scene");
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // string scene = PlayerPrefabs.GetString("scene");
+        // string weather = PlayerPrefabs.GetString("weather");
+        // string time = PlayerPrefabs.GetString("time");
+        // Debug.Log("VAMOS VER " + weather + " " + scene + " " + time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
